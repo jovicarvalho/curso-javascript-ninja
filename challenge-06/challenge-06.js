38d7a07c791ac5ddd67698893a77719fdc5c5b82
João Carvalho <54593758+jvncarvalho@users.noreply.github.com>
2022-05-09 03:28:45 -0300

    Finalização do Challenge 06

--- challenge-06/challenge-06.js
+++ challenge-06/challenge-06.js
@@ -4,7 +4,9 @@ para começar o desafio.
 Declare uma variável chamada `championship` que receberá o nome do campeonato,
 e imprima o nome desse campeonato no console.
 */
-// ?
+
+var campeonato = 'Série A';
+
 
 /*
 Declare uma variável chamada `teams`, que receberá um array com 5 elementos.
@@ -12,9 +14,10 @@ Os elementos serão nomes de times do campeonato escolhido, e os nomes devem
 estar na ordem em que eles aparecem na tabela no momento da solução desse
 desafio.
 */
-// ?
 
-console.log( 'Times que estão participando do campeonato:', teams );
+var times = ['A.S.Roma','Milan','Internacionale','Lazio','Fiorentina']
+
+console.log( 'Times que estão participando do campeonato:', times);
 
 /*
 Crie uma função chamada `showTeamPosition` com as seguintes características:
@@ -32,19 +35,42 @@ Crie uma função chamada `showTeamPosition` com as seguintes características:
     - Se não houver time para a posição passada, deve retornar a mensagem:
     "Não temos a informação do time que está nessa posição."
 */
-// ?
+
+function showTeamPosition(posicao){
+ var posicaoReal = posicao + 1; 
+ if(posicaoReal > 5 ){
+  return 'Não temos a infromação do time que está nessa posição';
+} 
+ return "O time que está em " + posicaoReal + " é o  " + times[posicao] + "."
+  
+  
+  
+}
+
 
 /*
 Escolha 4 times do campeonato selecionado e mostre a posição dele, usando a
 função acima. Entre esses 4, adicione 1 que não esteja entre os 5 primeiros.
 */
-// ?
+
+showTeamPosition(1)
+showTeamPosition(2)
+showTeamPosition(3)
+showTeamPosition(4)
+
+times[6] = "Napoli";
 
 /*
 Mostre os números de 20 a 30 no console (inclusive o 30), usando a estrutura de
 repetição "while".
 */
-// ?
+var contador = 0
+var numeroImpresso = 20
+while(contador <= 11){
+  console.log()
+  numeroImpresso++
+  contador++
+}
 
 /*
 Crie uma função chamada `convertToHex`, com as seguintes características:
@@ -58,9 +84,39 @@ Crie uma função chamada `convertToHex`, com as seguintes características:
     a frase:
     "Não temos o equivalente hexadecimal para [COR]."
 */
-// ?
+function convertToHex(cor){
+  switch(cor){
+    case 'red':
+      console.log('#ff00');
+      break;
+    case 'yellow':
+      console.log('#ffc222');
+      break;
+    case 'orange':
+      console.log('#ff7f00');
+      break;
+    case 'gray':
+      console.log('#808080');
+      break;
+    case 'black':
+      console.log('#000000');
+      break;
+    default:
+      console.log('Não temos o equivalente em hexadecimal para ' + cor +'.' )
+      }
+}
 
 /*
 Tente mostrar o hexadecimal de 8 cores diferentes usando a função criada acima.
 */
-// ?
+
+convertToHex('red');
+convertToHex('yellow');
+convertToHex('green');
+convertToHex('black');
+convertToHex('white');
+convertToHex('gray');
+convertToHex('orange');
+convertToHex('purple');
+convertToHex('brown');
+
